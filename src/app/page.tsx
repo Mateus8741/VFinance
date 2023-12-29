c2e886672d1165b771520a179ee5056d01abe7b1
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2023-12-29 16:13:09 -0300

    Add Card component to Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import { Card } from '@/components/Card/Card'
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 dark text-foreground bg-gray-900">
-      <div>teste</div>
+      <Card />
     </main>
   )
 }
