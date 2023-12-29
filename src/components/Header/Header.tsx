7d8c85df50132eba2923fe3781e3033461b24e85
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2023-12-29 17:48:04 -0300

    Add client-side functionality and modal component to Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,21 @@
+'use client'
+
+import { useDisclosure } from '@nextui-org/react'
 import { ArrowRightLeft } from 'lucide-react'
 import { Logo } from '../Logo/Logo'
+import { Modal } from '../Modal/Modal'
 
 export function Header() {
+  const { isOpen, onOpen, onOpenChange } = useDisclosure()
+
   return (
     <header className="flex items-center justify-between p-4 bg-gray-950">
       <Logo />
 
-      <button className="flex flex-row items-center justify-center gap-3 p-4 bg-gray-900 rounded-md">
+      <button
+        className="flex flex-row items-center justify-center gap-3 p-4 bg-gray-900 rounded-md"
+        onClick={onOpen}
+      >
         <div className="rotate-90">
           <ArrowRightLeft
             size="24"
@@ -18,6 +27,8 @@ export function Header() {
 
         <p className="text-sm font-bold text-green-700">Nova transação</p>
       </button>
+
+      <Modal isOpen={isOpen} onOpenChange={onOpenChange} />
     </header>
   )
 }
