48b54b68114e298de052dffaf9f83cd8a13d5972
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2023-12-29 17:29:34 -0300

    Update package.json and Card component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,12 @@ import { Card } from '@/components/Card/Card'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 dark text-foreground bg-gray-900">
-      <Card />
+    <main className="flex min-h-screen flex-col items-center justify-start p-24 dark text-foreground bg-gray-900">
+      <div className="flex flex-row items-center justify-between w-full">
+        <Card cardsType="inComing" value={1000} />
+        <Card cardsType="outComing" value={1000} />
+        <Card cardsType="total" value={1000} />
+      </div>
     </main>
   )
 }
--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,22 +1,85 @@
+import { formatMoney } from '@/utils/formatMoney'
 import { CardBody, Card as UICard } from '@nextui-org/react'
+import {
+  ArrowDownCircleIcon,
+  ArrowUpCircleIcon,
+  DollarSignIcon,
+} from 'lucide-react'
+import { VariantProps, tv } from 'tailwind-variants'
 
-import { ArrowUpCircleIcon } from 'lucide-react'
+const $cardStyle = tv({
+  slots: {
+    container: 'w-80 h-40 border-none isBlurred shadow-md',
+    text: 'text-base font-bold',
+  },
+  variants: {
+    cardsType: {
+      inComing: {
+        container: 'bg-background/60 dark:bg-default-100/50',
+        text: 'text-white',
+      },
+      outComing: {
+        container: 'bg-background/60 dark:bg-default-100/50',
+        text: 'text-white',
+      },
+      total: {
+        container: 'bg-green-600/70 dark:bg-green-600/50',
+        text: 'text-white',
+      },
+    },
+  },
 
-export function Card() {
+  defaultVariants: {
+    cardsType: 'inComing',
+  },
+})
+
+interface CardProps extends VariantProps<typeof $cardStyle> {
+  value: number
+  cardsType?: 'inComing' | 'outComing' | 'total'
+}
+
+export function Card({ value, cardsType = 'inComing' }: CardProps) {
+  const { container, text } = $cardStyle({ cardsType })
+
+  const formattedMoney = formatMoney(value)
+
+  let icon = null
+
+  switch (cardsType) {
+    case 'inComing':
+      icon = <ArrowUpCircleIcon size={32} color="green" />
+      break
+
+    case 'outComing':
+      icon = <ArrowDownCircleIcon size={32} color="red" />
+      break
+
+    case 'total':
+      icon = <DollarSignIcon size={32} color="white" />
+      break
+
+    default:
+      break
+  }
+
   return (
-    <UICard
-      className="w-80 h-40 border-none bg-background/60 dark:bg-default-100/50 "
-      isBlurred
-      shadow="md"
-    >
+    <UICard className={container()} isBlurred shadow="md">
       <CardBody className="flex justify-between p-3 py-5 text-small text-default-400">
         <div className="flex flex-row items-center justify-between">
-          <p className="text-green-700">Entradas</p>
-
-          <ArrowUpCircleIcon size={32} color="green" />
+          <p className={text()}>
+            {cardsType === 'inComing'
+              ? 'Entradas'
+              : cardsType === 'outComing'
+                ? 'Saídas'
+                : 'Total'}
+          </p>
+          {icon}
         </div>
 
-        <span className="pt-2 text-3xl font-bold text-green-700">R$ 0,00</span>
+        <span className={`pt-2 text-3xl font-bold ${text()}`}>
+          {formattedMoney}
+        </span>
       </CardBody>
     </UICard>
   )
