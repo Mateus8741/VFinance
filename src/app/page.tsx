bc23757d6636d79c1935a8fbb2d55794b17909c3
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2024-01-05 08:46:34 -0300

    Add Suspense component to improve loading experience

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,17 +2,20 @@
 
 import { Card } from '@/components/Card/Card'
 import { useSummary } from '@/hooks/useSummary'
+import { Suspense } from 'react'
 
 export default function Home() {
   const { income, outcome, total } = useSummary()
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-start p-24 dark text-foreground bg-gray-900">
-      <div className="flex flex-row items-center justify-between w-full">
-        <Card cardsType="inComing" value={income} />
-        <Card cardsType="outComing" value={outcome} />
-        <Card cardsType="total" value={total} />
-      </div>
+      <Suspense fallback={<div>Loading...</div>}>
+        <div className="flex flex-row items-center justify-between w-full">
+          <Card cardsType="inComing" value={income} />
+          <Card cardsType="outComing" value={outcome} />
+          <Card cardsType="total" value={total} />
+        </div>
+      </Suspense>
     </main>
   )
 }
